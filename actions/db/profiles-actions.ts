6205793d36d031fe77a98f131ba6be4f09ff7a69
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2024-10-10 16:16:56 -0600

    add console errors

--- actions/db/profiles-actions.ts
+++ actions/db/profiles-actions.ts
@@ -1,25 +1,42 @@
-"use server";
+"use server"
 
-import { createProfile, getProfileByUserId } from "@/db/queries/profiles-queries";
-import { InsertProfile, SelectProfile } from "@/db/schema/profiles-schema";
-import { ActionState } from "@/types";
-import { revalidatePath } from "next/cache";
+import {
+  createProfile,
+  getProfileByUserId
+} from "@/db/queries/profiles-queries"
+import { InsertProfile, SelectProfile } from "@/db/schema/profiles-schema"
+import { ActionState } from "@/types"
+import { revalidatePath } from "next/cache"
 
-export async function createProfileAction(data: InsertProfile): Promise<ActionState<SelectProfile>> {
+export async function createProfileAction(
+  data: InsertProfile
+): Promise<ActionState<SelectProfile>> {
   try {
-    const newProfile = await createProfile(data);
-    revalidatePath("/");
-    return { isSuccess: true, message: "Profile created successfully", data: newProfile };
+    const newProfile = await createProfile(data)
+    revalidatePath("/")
+    return {
+      isSuccess: true,
+      message: "Profile created successfully",
+      data: newProfile
+    }
   } catch (error) {
-    return { isSuccess: false, message: "Failed to create profile" };
+    console.error("Error creating profile", error)
+    return { isSuccess: false, message: "Failed to create profile" }
   }
 }
 
-export async function getProfileByUserIdAction(userId: string): Promise<ActionState<SelectProfile>> {
+export async function getProfileByUserIdAction(
+  userId: string
+): Promise<ActionState<SelectProfile>> {
   try {
-    const profile = await getProfileByUserId(userId);
-    return { isSuccess: true, message: "Profile retrieved successfully", data: profile };
+    const profile = await getProfileByUserId(userId)
+    return {
+      isSuccess: true,
+      message: "Profile retrieved successfully",
+      data: profile
+    }
   } catch (error) {
-    return { isSuccess: false, message: "Failed to get profile" };
+    console.error("Error getting profile by user id", error)
+    return { isSuccess: false, message: "Failed to get profile" }
   }
 }
