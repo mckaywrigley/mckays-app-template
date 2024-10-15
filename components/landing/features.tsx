3bc83454b2b9a40f4bd4177a7df260e5a04056fe
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2024-10-14 20:48:10 -0600

    add tech stack

--- components/landing/features.tsx
+++ components/landing/features.tsx
@@ -1,7 +1,13 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { Headphones, LucideIcon, Shield, Smartphone, Zap } from "lucide-react"
+import {
+  AppWindow,
+  Database,
+  DollarSign,
+  LucideIcon,
+  Shield
+} from "lucide-react"
 
 interface FeatureProps {
   title: string
@@ -11,24 +17,24 @@ interface FeatureProps {
 
 const features: FeatureProps[] = [
   {
-    title: "Lightning Fast",
-    description: "Experience blazing speeds with our optimized platform.",
-    icon: Zap
+    title: "Frontend",
+    description: "Next.js, Tailwind, Shadcn, Framer Motion",
+    icon: AppWindow
   },
   {
-    title: "Secure",
-    description: "Your data is protected with state-of-the-art encryption.",
+    title: "Backend",
+    description: "Postgres, Supabase, Drizzle ORM, Server Actions",
+    icon: Database
+  },
+  {
+    title: "Auth",
+    description: "Clerk",
     icon: Shield
   },
   {
-    title: "Mobile Friendly",
-    description: "Access your account seamlessly on any device.",
-    icon: Smartphone
-  },
-  {
-    title: "24/7 Support",
-    description: "Our dedicated team is always here to help you.",
-    icon: Headphones
+    title: "Payments",
+    description: "Stripe",
+    icon: DollarSign
   }
 ]
 
@@ -46,9 +52,9 @@ const FeatureCard = ({ title, description, icon: Icon }: FeatureProps) => (
 
 export const FeaturesSection = () => {
   return (
-    <section className="bg-gray-100 py-20 dark:bg-gray-900">
+    <section className="mt-20 bg-gray-100 py-20 dark:bg-gray-900">
       <div className="container mx-auto px-4">
-        <h2 className="mb-12 text-center text-4xl font-bold">Our Features</h2>
+        <h2 className="mb-12 text-center text-4xl font-bold">Tech Stack</h2>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
           {features.map((feature, index) => (
             <FeatureCard key={index} {...feature} />
