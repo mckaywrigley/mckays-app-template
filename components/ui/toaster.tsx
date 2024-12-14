4efbe328b62a65ab31abb80efd014a0a9e610931
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2024-12-14 13:42:31 -0700

    new component structure

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -8,7 +8,7 @@ import {
   ToastTitle,
   ToastViewport
 } from "@/components/ui/toast"
-import { useToast } from "@/lib/hooks/use-toast"
+import { useToast } from "@/components/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
